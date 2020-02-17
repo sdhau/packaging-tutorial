2dda911f4ee56ca36500652dd6daa7b62d565686
Sulav Dhaubhadel <sulav.dhaubhadel@perficient.com>
2020-02-17 11:35:16 -0600

    example pacakge

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="package",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
